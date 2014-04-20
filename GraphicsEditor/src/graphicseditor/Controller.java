58ce5e1ef052b97470812b88b990750d7bda7191
Alex <if12b062@technikum-wien.at>
2014-04-20 17:06:24 +0200

    Added first mouselistener

--- GraphicsEditor/src/graphicseditor/Controller.java
+++ GraphicsEditor/src/graphicseditor/Controller.java
@@ -1,15 +1,10 @@
 package graphicseditor;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.*;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.ArcType;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -25,9 +20,7 @@ public class Controller implements Initializable{
 
    @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-       canvas = new Canvas(300, 250);
-       canvasPane.setContent(canvas);
-       GraphicsContext gc = canvas.getGraphicsContext2D();
+
     }
 
 
--- GraphicsEditor/src/graphicseditor/Main.java
+++ GraphicsEditor/src/graphicseditor/Main.java
@@ -1,12 +1,18 @@
 package graphicseditor;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
@@ -14,13 +20,33 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Parent root;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("editor.fxml"));
+        root = FXMLLoader.load(getClass().getResource("editor.fxml"));
         primaryStage.setTitle("Graphics Editor");
-        ScrollPane canvasPane = (ScrollPane) root.lookup("#canvasPane");
+        StackPane canvasPane = (StackPane) root.lookup("#canvasPane");
+        canvasPane.getChildren().add(setUpCanvas(primaryStage));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    private Canvas setUpCanvas(Stage primaryStage) {
+        final Canvas canvas = new Canvas();
+        canvas.widthProperty().bind(primaryStage.widthProperty());
+        canvas.heightProperty().bind(primaryStage.heightProperty());
+        canvas.setCursor(Cursor.CROSSHAIR);
+        canvas.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                ColorPicker colorPicker = (ColorPicker) root.lookup("#colorPicker");
+                GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+                graphicsContext.setFill(colorPicker.getValue());
+                graphicsContext.fillRect(me.getX(), me.getY(), 20, 20);
+            }
+        });
+        return canvas;
     }
 
 
